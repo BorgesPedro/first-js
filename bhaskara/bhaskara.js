3ce0838dca2443d5c53e21d584f79c49ada8a102
Pedro Borges <BorgesPedro@users.noreply.github.com>
2021-01-10 12:35:42 -0300

    Taking the numbers for delta

---
+++ bhaskara/bhaskara.js
@@ -0,0 +1,9 @@
+var a = Number(document.getElementById('avalue').value)
+var b = Number(document.getElementById('bvalue').value)
+var c = Number(document.getElementById('cvalue').value)
+
+function deltaCalc() {
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
