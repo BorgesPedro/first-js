b15eaf19a50050ea746a9aa6c27228faeaaa251d
Pedro Borges <BorgesPedro@users.noreply.github.com>
2021-01-17 11:38:20 -0300

    update

--- bhaskara/bhaskara.js
+++ bhaskara/bhaskara.js
@@ -1,9 +1,15 @@
 var a = Number(document.getElementById('avalue').value)
 var b = Number(document.getElementById('bvalue').value)
 var c = Number(document.getElementById('cvalue').value)
+var result = document.getElementById('result')
 
 function deltaCalc() {
-    console.log(a)
-    console.log(b)
-    console.log(c)
+    let resultado = Math.pow(b,2) - 4*a*c
+    result.innerText = `O valor de delta é: ${resultado}`
+    return resultado
+}
+
+function calcularBhaskara() {
+    let resultadoBhaskara = (-b + Math.sqrt(deltaCalc())) / (2*a)
+    console.log(resultadoBhaskara)
 }
